e269e2d73e31c7e60d1f0a1ea62339a73420a882
mousamdhakal <mousamdhakal7.md@gmail.com>
2020-07-29 09:46:12 +0545

    Updates

---
+++ precache-manifest.e538219d438e8065fd12fb71ac959859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a942a740629fe598bf7353ea2d8ac6b",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "e2dff875bd9c7dbdd367",
+    "url": "/todo/static/css/main.d8c7d56e.chunk.css"
+  },
+  {
+    "revision": "044f73c19b92671b7d6d",
+    "url": "/todo/static/js/2.c0264c78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.c0264c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2dff875bd9c7dbdd367",
+    "url": "/todo/static/js/main.b12840fb.chunk.js"
+  },
+  {
+    "revision": "10fd3631c43d335a3db9",
+    "url": "/todo/static/js/runtime-main.2a8ec137.js"
+  }
+]);
